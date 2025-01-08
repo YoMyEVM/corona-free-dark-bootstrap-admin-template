8d72a351b2637dff25f38af482149a1b2cdd5dda
Fun Fund <yo.myevm@gmail.com>
2025-01-08 03:52:44 +0000

    .

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,13 +1,25 @@
-'use strict'
-var gulp = require('gulp');
-var requireDir = require('require-dir');
-requireDir('gulp-tasks');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 
+// Sass task
+gulp.task('sass', () => {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
 
-gulp.paths = {
-    dist: 'dist',
-};
+// Serve task using BrowserSync
+gulp.task('serve', () => {
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
 
-var paths = gulp.paths;
-
-gulp.task('default', gulp.series('serve'));
+// Default task
+gulp.task('default', gulp.series('sass', 'serve'));
